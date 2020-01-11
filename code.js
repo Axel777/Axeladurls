6246fd11c1267a541f317987714520ccaa289516
Axel777 <thedeveloperboy@gmail.com>
2020-01-11 14:55:09 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,25 +1,5 @@
-<!-- PopAds.net Popunder Code for pornxxxhd.net -->
-<script type="text/javascript" data-cfasync="false">
-/*<![CDATA[/* */
-  var _pop = _pop || [];
-  _pop.push(['siteId', 3640896]);
-  _pop.push(['minBid', 0]);
-  _pop.push(['popundersPerIP', 0]);
-  _pop.push(['delayBetween', 0]);
-  _pop.push(['default', false]);
-  _pop.push(['defaultPerDay', 0]);
-  _pop.push(['topmostLayer', false]);
-  (function() {
-    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
-    var s = document.getElementsByTagName('script')[0]; 
-    pa.src = '//c1.popads.net/pop.js';
-    pa.onerror = function() {
-      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
-      sa.src = '//c2.popads.net/pop.js';
-      s.parentNode.insertBefore(sa, s);
-    };
-    s.parentNode.insertBefore(pa, s);
-  })();
-/*]]>/* */
+<script type="text/javascript">
+  var uid = '253612';
+  var wid = '523444';
 </script>
-<!-- PopAds.net Popunder Code End -->
+<script type="text/javascript" src="//cdn.popcash.net/pop.js"></script>
